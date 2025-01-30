db53ac95f51a76af1dc78a9c8c829547586db5ac
emmanuelologe <epicraez>
2025-01-30 07:28:29 +0100

    server

--- Contacts-api/server.js
+++ Contacts-api/server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
 
-const contactRoutes = require("./contacts.js");
+const contactRoutes = require("./routes/contacts.js");
 
 app.use("/contacts", contactRoutes);
 
